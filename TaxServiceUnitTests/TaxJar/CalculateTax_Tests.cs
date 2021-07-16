effdfa170cd4a5b0a6c419d7cbd14900de715e20
Justin LaRose <jlarose@imcdi.com>
2021-07-16 10:46:30 -0400

    Finished cleaning up the Unit Test standards and moving over to Xunit

--- TaxServiceUnitTests/TaxJar/CalculateTax_Tests.cs
+++ TaxServiceUnitTests/TaxJar/CalculateTax_Tests.cs
@@ -23,46 +23,65 @@ namespace TaxServiceUnitTests
                 taxServiceTaxJar = new TaxService.TaxService(TaxService.Models.TaxCalculators.TaxJar, mockTaxJarDAL.Object);
             }
 
+            public TaxJarCalculateTax_Model getValidMockCalculateTaxModel()
+            {
+                return new TaxJarCalculateTax_Model()
+                {
+                    Amount = 15,
+                    ToCountry = "US",
+                    ToZip = "90002",
+                    ToState = "CA",
+                    Shipping = 1.5m,
+                    FromCity = "La Jolla",
+                    FromCountry = "US",
+                    FromState = "CA",
+                    FromStreet = "9500 Gilman Drive",
+                    FromZip = "92093"
+                };
+            }
+
+            public TaxJarCalculateTaxResponse_Model getMockCalculateTaxResponse()
+            {
+                return new TaxJarCalculateTaxResponse_Model()
+                {
+                    Tax = new Tax()
+                    {
+                        AmountToCollect = 1.54m
+                    }
+                };
+            }
+
         }
 
+        [Fact(DisplayName = "ValidTaxCalculatorRequest")]
+        [Trait("Category", "UnitTest")]
         public async Task ValidTaxCalculatorRequest()
         {
             var setup = new Setup();
-            setup.mockTaxJarDAL.Setup(x => x.CalculateTax(It.IsAny<TaxJarCalculateTax_Model>())).Returns(Task.FromResult(It.IsAny<TaxJarCalculateTaxResponse_Model>()));
-            
+            var mockCalculateTaxRequest = setup.getValidMockCalculateTaxModel();
+            var mockCalculateTaxResponse = setup.getMockCalculateTaxResponse();
 
-            TaxJarCalculateTax_Model request = new TaxJarCalculateTax_Model
-            {
-                Amount = 15,
-                ToCountry = "US",
-                ToZip = "90002",
-                ToState = "CA",
-                Shipping = 1.5m,
-                FromCity = "La Jolla",
-                FromCountry = "US",
-                FromState = "CA",
-                FromStreet = "9500 Gilman Drive",
-                FromZip = "92093"
-            };
+            setup.mockTaxJarDAL.Setup(x => x.CalculateTax(It.IsAny<TaxJarCalculateTax_Model>())).Returns(Task.FromResult(mockCalculateTaxResponse));
 
-            decimal response = await setup.taxServiceTaxJar.CalculateTax(request);
-
-            Assert.NotNull(response);
+            decimal response = await setup.taxServiceTaxJar.CalculateTax(mockCalculateTaxRequest);
 
             // Value determined via independent test from a Postman request 
             Assert.Equal(1.54m, response);
         }
 
-        //[TestMethod]
-        //[ExpectedException(typeof(Exception))]
+        [Fact(DisplayName = "InvalidTaxCalculatorRequest")]
+        [Trait("Category", "UnitTest")]
         public async Task InvalidTaxCalculatorRequest()
         {
             var setup = new Setup();
-            setup.mockTaxJarDAL.Setup(x => x.CalculateTax(It.IsAny<TaxJarCalculateTax_Model>())).Returns(Task.FromResult(It.IsAny<TaxJarCalculateTaxResponse_Model>()));
-            // No request object passed in, returns exception
-            decimal response = await setup.taxServiceTaxJar.CalculateTax();
+            var mockCalculateTaxResponse = setup.getMockCalculateTaxResponse();
 
-            await Assert.ThrowsAsync<Exception>(() => setup.taxServiceTaxJar.CalculateTax());
+            setup.mockTaxJarDAL.Setup(x => x.CalculateTax(It.IsAny<TaxJarCalculateTax_Model>())).Returns(Task.FromResult(mockCalculateTaxResponse));
+
+            // No request object passed in, returns exception
+            Exception ex = await Assert.ThrowsAsync<Exception>(() => setup.taxServiceTaxJar.CalculateTax());
+
+            Assert.Equal("Invalid TaxJar Calculate Tax model provided for Request.", ex.Message);
         }
 
     }
--- TaxServiceUnitTests/TaxJar/GetLocationTaxRates_Tests.cs
+++ TaxServiceUnitTests/TaxJar/GetLocationTaxRates_Tests.cs
@@ -22,79 +22,97 @@ namespace TaxServiceUnitTests
                 taxServiceTaxJar = new TaxService.TaxService(TaxService.Models.TaxCalculators.TaxJar, mockTaxJarDAL.Object);
             }
 
-            public TaxJarRatesResponse_Model getMockTaxJarRatesResponse(string zip)
+            public TaxJarRatesResponse_Model getMockTaxJarRatesResponse(string zip, decimal expectedRateResponse)
             {
                 TaxJarRatesResponse_Model response = new TaxJarRatesResponse_Model()
                 {
                     Rate = new RatesResponseAttributes()
                     {
-                        StandardRate = 0.055m,
+                        CombinedRate = expectedRateResponse,
                         Zip = zip
                     }
                 };
 
                 return response;
             }
+
+            public TaxJarRatesRequest_Model getMockTaxJarRatesRequest (string zip = null, string state = null, string city = null, string country = null, string street = null)
+            {
+                return new TaxJarRatesRequest_Model()
+                {
+                    ZIP = zip,
+                    State = state,
+                    City = city,
+                    Country = country,
+                    Street = street
+                };
+            }
         }
 
         //[TestMethod]
+        [Fact(DisplayName = "ValidLocationTaxRatesRequest")]
+        [Trait("Category", "UnitTest")]
         public async Task ValidLocationTaxRatesRequest()
         {
-            TaxJarRatesRequest_Model request = new TaxJarRatesRequest_Model
-            {
-                ZIP = "04062"
-            };
-
             Setup setup = new Setup();
-            var taxJarRatesResponse = setup.getMockTaxJarRatesResponse("04062");
+            var taxJarRatesResponse = setup.getMockTaxJarRatesResponse("04062", 0.055m);
+            var mockRatesRequest = setup.getMockTaxJarRatesRequest("04062");
+
             setup.mockTaxJarDAL.Setup(x => x.GetLocationTaxRates(It.IsAny<string>())).Returns(Task.FromResult(taxJarRatesResponse));
             
 
-            decimal response = await setup.taxServiceTaxJar.GetLocationTaxRates(request);
+            decimal response = await setup.taxServiceTaxJar.GetLocationTaxRates(mockRatesRequest);
 
             Assert.Equal(0.055m, response);
         }
 
         //[TestMethod]
         //[ExpectedException(typeof(Exception))]
+        [Fact(DisplayName = "LocationTaxRatesRequest_NoZIP")]
+        [Trait("Category", "UnitTest")]
         public async Task LocationTaxRatesRequest_NoZIP()
         {
-            // No ZIP defined, it is a required field. Should throw Exception
-            TaxJarRatesRequest_Model request = new TaxJarRatesRequest_Model();
 
-            decimal response = await taxServiceTaxJar.GetLocationTaxRates(request);
+            Setup setup = new Setup();
+            var taxJarRatesResponse = setup.getMockTaxJarRatesResponse("04062", 0.055m);
+            var mockRatesRequest = setup.getMockTaxJarRatesRequest();
+
+            setup.mockTaxJarDAL.Setup(x => x.GetLocationTaxRates(It.IsAny<string>())).Returns(Task.FromResult(taxJarRatesResponse));
+            
+            // No ZIP defined, it is a required field. Should throw Exception
+            await Assert.ThrowsAsync<Exception>(() =>  setup.taxServiceTaxJar.GetLocationTaxRates(mockRatesRequest));
         }
 
         //[TestMethod]
         //[ExpectedException(typeof(Exception))]
+        [Fact(DisplayName = "LocationTaxRatesRequest_NoZIPButOtherInfo")]
+        [Trait("Category", "UnitTest")]
         public async Task LocationTaxRatesRequest_NoZIPButOtherInfo()
         {
-            // No ZIP defined, it is a required field. Should throw Exception
-            TaxJarRatesRequest_Model request = new TaxJarRatesRequest_Model
-            {
-                Country = "US",
-                State = "ME"
-            };
+            Setup setup = new Setup();
+            var taxJarRatesResponse = setup.getMockTaxJarRatesResponse("04062", 0.055m);
+            var mockRatesRequest = setup.getMockTaxJarRatesRequest(null, "ME", null, "US");
 
-            decimal response = await taxServiceTaxJar.GetLocationTaxRates(request);
+            setup.mockTaxJarDAL.Setup(x => x.GetLocationTaxRates(It.IsAny<string>())).Returns(Task.FromResult(taxJarRatesResponse));
+
+            // No ZIP defined, it is a required field. Should throw Exception
+            await Assert.ThrowsAsync<Exception>(() => setup.taxServiceTaxJar.GetLocationTaxRates(mockRatesRequest));
         }
 
         //[TestMethod]
+        [Fact(DisplayName = "LocationTaxRatesRequest_FullInfo")]
+        [Trait("Category", "UnitTest")]
         public async Task LocationTaxRatesRequest_FullInfo()
-        {
-            TaxJarRatesRequest_Model request = new TaxJarRatesRequest_Model
-            {
-                ZIP = "04062",
-                City = "Windham",
-                Country = "US",
-                State = "ME",
-                Street = "73 Whites Bridge Road"
-            };
+        {           
+            Setup setup = new Setup();
+            var taxJarRatesResponse = setup.getMockTaxJarRatesResponse("04062", 0.055m);
+            var mockRatesRequest = setup.getMockTaxJarRatesRequest("04062", "ME", "Windham", "US", "73 Whites Bridge Road");
 
-            decimal response = await taxServiceTaxJar.GetLocationTaxRates(request);
+            setup.mockTaxJarDAL.Setup(x => x.GetLocationTaxRates(It.IsAny<string>())).Returns(Task.FromResult(taxJarRatesResponse));
 
-            // Value determined via independent test from a Postman request
-            Assert.AreEqual(0.055m, response);
+            decimal response = await setup.taxServiceTaxJar.GetLocationTaxRates(mockRatesRequest);
+
+            Assert.Equal(0.055m, response);
 
         }
     }
